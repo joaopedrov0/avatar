abcd6b670b1613d937f00e6c727874f08bb4b7d1
João Pedro V <mastergamerjp06@gmail.com>
2024-01-02 00:06:10 -0300

    muita coisa

--- javascript/fire-skilltree.js
+++ javascript/fire-skilltree.js
@@ -13,7 +13,7 @@
 //     castingTime: '',
 //     description: '',
 //     price: 'xp required',
-//     purchased: false or true,
+//     purchased: {} or true,
 //     upgrades: {
 //         atribute: [level, price1, price2, price3...]
 //     },
@@ -22,7 +22,7 @@
 
 class FireSkillTree {
     basicAttack = {
-        nome: 'Ataque Básico',
+        name: 'Ataque Básico',
         range: [
             5,
             6,
@@ -44,14 +44,14 @@ class FireSkillTree {
         castingTime: '1 ação',
         description: 'O dominador lança uma chama sobre o inimigo até 7,5 metros, causando 1d8 + MF de dano. A cada 1,5 metros (1 quadrado)  além do limite de distância seu ataque dará metade do dano arredondado para baixo com limite mínimo de um de dano.',
         price: '0',
-        purchased: false,
+        purchased: {damage: 0, range: 0},
         upgrades: {
             damage: [0, 1700, 2000, 2300, 2600],
             range: [0, 1800, 2100, 2400, 2700],
         },
     }
     fireBall = {
-        nome: 'Bola de Fogo',
+        name: 'Bola de Fogo',
         range: [
             7,
             8,
@@ -71,14 +71,14 @@ class FireSkillTree {
         castingTime: '1 ação',
         description: 'Uma bola de fogo sairá de uma das mãos do dominador chegando até 10,5 metros, queimando o inimigo, causando 2d6 + MF de dano.',
         price: '600',
-        purchased: false,
+        purchased: {damage: 0, range: 0},
         upgrades: {
             damage: [0, 75, 150, 150, 150],
             range: [0, 150, 150],
         },
     }
     flamingKick = {
-        nome: 'Chute Flamejante',
+        name: 'Chute Flamejante',
         range: [
             3,
             4,
@@ -98,14 +98,14 @@ class FireSkillTree {
         castingTime: '1 ação bônus',
         description: 'Com uma ação bônus, o dobrador no meio de suas ações faz um chute lateral causando 1d4 + MF nas pessoas que estiverem em sua frente em até 4,5 metros.',
         price: '400',
-        purchased: false,
+        purchased: {damage: 0, range: 0},
         upgrades: {
             damage: [0, 100, 100, 150, 200],
             range: [0, 150, 150],
         },
     }
     canalization = {
-        nome: 'Canalização',
+        name: 'Canalização',
         range: [
             8,
             9,
@@ -125,7 +125,7 @@ class FireSkillTree {
         castingTime: '2 ações (mínimo)',
         description: 'Canaliza a bola de fogo com as duas mãos, por meio de, inicialmente, duas ações, lançando uma bola de fogo com mais energia até 12 metros, causando 4d6 + 2d4 + MF em uma pessoa. A cada ação adicional que for utilizada, dará 2d6 + 3d4 por ação. Além disso, a partir da 3° ação utilizada, o dominador terá + 3 para acertar. Ataques recebidos durante a canalização a cancelam. É preciso ter visto o inimigo para utilizar este ataque.',
         price: '900',
-        purchased: false,
+        purchased: {damage: 0, range: 0},
         upgrades: {
             damage: [0, 175, 225, 150, 400],
             range: [0, 150, 150],
@@ -144,8 +144,8 @@ class FireSkillTree {
         castingTime: '1 ação',
         description:
           'O dobrador de fogo lança com uma das mãos uma labareda de fogo constante, de até 9 metros, causando 4d4 de dano e aplicando Feridas Dolorosas durante um turno após o recebimento do dano. Inimigos sob este efeito recebem + ⅓ do dano total que lhe foi causado por esta habilidade. Este efeito não se acumula.',
-        xp: 700,
-        purchased: false,
+        price: 700,
+        purchased: {damage: 0, range: 0},
         upgrades: {
           damage: [0, 100, 150, 350, 200],
           range: [0, 150, 150],
@@ -155,7 +155,7 @@ class FireSkillTree {
     fireWall = {
         name: 'Parede de Fogo',
         range: [5, 6, 7], // 5 quadrados = 7,5 metros
-        damage: ['2MF' ,'3MF'],
+        damage: ['1MF' ,'2MF'],
         wallSize: [5, 6, 7, 8, 9],
         learningComponents: 'Nenhum',
         duration: '12 segundos, 2 turnos',
@@ -163,8 +163,8 @@ class FireSkillTree {
         toHit: '1d20',
         castingTime: '1 ação',
         description:'O dobrador, atira no chão diversas brasas interligadas, após isso ele aumenta o tamanho da mesma criando uma parede de fogo de no máximo 7,5 metros de distância com 2 metros de altura, ele poderá conjurar essa parede até 7,5 metros dele, caso precise, poderá se movimentar para completar o formato da parede. Caso alguém passe pela parede, ela causará dano nesta pessoa. Dominadores de água e de ar podem tentar apagar a parede com habilidades plausíveis ou ataques básicos. Esta habilidade dura dois turnos.',
-        xp: 700,
-        purchased: false,
+        price: 700,
+        purchased: {damage: 0, range: 0, wallSize: 0},
         upgrades: {
             damage: [0, 300],
             range: [0, 150, 150],
@@ -181,9 +181,9 @@ class FireSkillTree {
         uses: 2,
         toHit: '1d20',
         castingTime: '1 reação',
-        defender: '1d20 + MD + 3',
-        xp: 800,
-        purchased: false,
+        defender: ['1d20 + 3', '1d20 + 4', '1d20 + 5', '1d20 + 6'],
+        price: 800,
+        purchased: {damage: 0, range: 0, defender: 0},
         upgrades: {
             damage: [0, 100, 150, 200],
             range: [0, 150, 150],
--- javascript/sheet.js
+++ javascript/sheet.js
@@ -1,4 +1,5 @@
 let PersonagensList
+let skillTree
 async function requestPersonagens() {
     let { data: Personagens, error } = await supabaseClient
     .from('Personagens')
@@ -42,6 +43,7 @@ function toggleCard(skill){
 
 
 function openSheet(id){
+    skillTree = ''
 
     let character = JSON.parse(PersonagensList.find((personagem) => personagem.id === id).sheet)
 
@@ -63,10 +65,10 @@ function openSheet(id){
                             </div>
                             <div class="life-number"><span contenteditable="true">${character.life}</span>/${character.life}</div>
                             <div class="life-buttons">
-                                <span class="material-symbols-outlined">
+                                <span class="material-symbols-outlined" onclick="changeLifePoints(true)">
                                     add
                                 </span>
-                                <span class="material-symbols-outlined">
+                                <span class="material-symbols-outlined" onclick="changeLifePoints(false)">
                                     remove
                                 </span>
                             </div>
@@ -106,9 +108,10 @@ function openSheet(id){
                     </div>
                     <div class="skills">
                         <div class="owned">
-
+                        <h2>Compradas</h2>
                         </div>
                         <div class="available">
+                        <h2>Disponíveis</h2>
                         
                         </div>
                     </div>
@@ -119,4 +122,115 @@ function openSheet(id){
 
 
     outside.classList.toggle('visible')
+
+    renderSkills(character)
+}
+
+function renderSkills(character) {
+    skillTree = new FireSkillTree()
+    console.log(skillTree)
+
+    let owned = document.querySelector('.owned')
+    let available = document.querySelector('.available')
+
+
+    for (skill in skillTree){
+        if(character.skills[skill]){
+            owned.innerHTML += createSkillCard(character.skills[skill], skill, character)
+        } else {
+            available.innerHTML += createSkillCard(skillTree[skill], skill)
+        }
+    }
+}
+
+function createSkillCard(skill, key, character){
+    let temp = ''
+    temp += `
+    <div class="skill-card reduced" id="${key}">
+        <div class="skill-header">
+            <div class="skill-name"><strong>${skill.name}</strong></div>
+            <div class="skill-price"><strong>Preço:</strong> ${skill.price}xp</div>
+            <div class="skill-expand-btn" onclick="toggleCard('${key}')">
+                <span class="material-symbols-outlined">
+                    expand_more
+                </span>
+            </div>
+        </div>
+        <div class="skill-content">
+        `
+    if(skill.castingTime){
+        temp += `<div class="skill-castingTime"><strong>Tempo de conjuração:</strong> ${skill.castingTime}</div>`
+    }
+    if(skill.range){
+        temp += `<div class="skill-range"><strong>Alcance:</strong> ${skill.range[skill.purchased.range]}</div>`
+    }
+    if(skill.learningComponents){
+        temp += `<div class="skill-learningComponents"><strong>Componentes de aprendizagem:</strong> ${skill.learningComponents}</div>`
+    }
+    if(skill.duration){
+        temp += `<div class="skill-duration"><strong>Duração:</strong> ${skill.duration}</div>`
+    }
+    if(skill.damage && character){
+        temp += `<div class="skill-damage"><strong>Dano:</strong> ${skill.damage[skill.purchased.damage]} + ${Math.floor((parseInt(character.str) - 10) / 2)}</div>`
+    } else if (skill.damage){
+        temp += `<div class="skill-damage"><strong>Dano:</strong> ${skill.damage[skill.purchased.damage]} + MF</div>`
+    }
+
+    if(skill.defender && character){
+        temp += `<div class="skill-damage"><strong>Defesa:</strong> ${skill.defender[skill.purchased.defender]} + ${Math.floor((parseInt(character.dex) - 10) / 2)}</div>`
+    } else if (skill.defender){
+        temp += `<div class="skill-damage"><strong>Dano:</strong> ${skill.defender[skill.purchased.defender]} + MD</div>`
+    }
+    if(skill.uses){
+        temp += `<div class="skill-uses"><strong>Usos:</strong> <span contenteditable="true">${skill.uses}</span>/${skill.uses}</div>`
+    }
+        temp +=`
+            <div class="skill-description"><strong>Descrição:</strong> ${skill.description}</div>
+        </div>
+        <div class="skill-upgrades">
+        <strong>Upgrades</strong>
+        `
+        if(skill.upgrades.damage){
+            temp +=`<div class="skill-upgrades-damage"><strong>Dano:</strong> Nível ${skill.purchased.damage}</div>`
+        }
+        if(skill.upgrades.range){
+            temp +=`<div class="skill-upgrades-range"><strong>Alcance:</strong> Nível ${skill.purchased.range}</div>`
+        }
+        if(skill.upgrades.wallSize){
+            temp +=`<div class="skill-upgrades-wallSize"><strong>Tamanho da parede:</strong> Nível ${skill.purchased.wallSize}</div>`
+        }
+        if(skill.upgrades.defender){
+            temp +=`<div class="skill-upgrades-defender"><strong>Defender:</strong> Nível ${skill.purchased.defender}</div>`
+        }
+        if(skill.upgrades.difficultyClass){
+            temp +=`<div class="skill-upgrades-difficultyClass"><strong>Classe de dificuldade:</strong> Nível ${skill.purchased.difficultyClass}</div>`
+        }
+        if(!character){
+            temp +=`<div class="life-btn avatar-airbender" onclick="buySkill(${key})">
+                        Comprar<br>${skill.price}xp
+                    </div>`
+        }
+        `
+
+        </div>
+    </div>
+    `
+
+    return temp
+}
+
+
+
+function changeLifePoints(signal){
+    let currentHP = document.querySelector('.life-number span')
+    if(signal){
+        currentHP.innerText = parseInt(currentHP.innerText) + 1
+    } else {
+        currentHP.innerText = parseInt(currentHP.innerText) - 1
+    }
+}
+
+
+function buySkill(){
+    alert('Função pendente')
 }
